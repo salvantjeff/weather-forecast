3d048fc87567d6e09ddc920a57ebc3966f9d4ee9
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-13 20:12:51 -0500

    Add location content

--- weather-forecast-app/src/components/Main/Main.js
+++ weather-forecast-app/src/components/Main/Main.js
@@ -1,9 +1,14 @@
 import './Main.css';
 import Today from './Today/Today';
+import { GoLocation } from 'react-icons/go';
 
 function Main() {
     return (
         <main className='main-section'>
+            <div className='location-content'>
+                <p>Richmond, VA</p>
+                <GoLocation />
+            </div>
             <Today />
         </main>
     );
