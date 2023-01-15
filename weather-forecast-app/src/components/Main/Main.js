0c259d58322d0e00053ed05b46fdb990e85cfa1d
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-15 13:12:22 -0500

    Set the location

--- weather-forecast-app/src/components/Main/Main.js
+++ weather-forecast-app/src/components/Main/Main.js
@@ -3,12 +3,18 @@ import Today from './Today/Today';
 import { GoLocation } from 'react-icons/go';
 import Weekly from './Weekly/Weekly';
 import Hourly from './Hourly/Hourly';
+import initWeatherData from '../../data/initWeatherData';
 
 function Main({ weatherData }) {
+    let today = initWeatherData.today;
+    if (weatherData.today) {
+        today = {...weatherData.today};
+    };
+
     return (
         <main className='main-section'>
             <div className='location-content'>
-                <p>Richmond, VA</p>
+                <p>{today.city}, {today.country}</p>
                 <GoLocation />
             </div>
             <div className='main-content'>
