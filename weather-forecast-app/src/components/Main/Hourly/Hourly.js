7e432f0395d88d6c3cb00f3d52fe4465cc822949
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-15 09:48:05 -0500

    Convert default metric units

--- weather-forecast-app/src/components/Main/Hourly/Hourly.js
+++ weather-forecast-app/src/components/Main/Hourly/Hourly.js
@@ -2,6 +2,7 @@ import './Hourly.css';
 import { BsFillCloudSnowFill, BsWind } from 'react-icons/bs';
 import { WiHumidity } from 'react-icons/wi';
 import initWeatherData from '../../../data/initWeatherData';
+import { kelvinToFahrenheit, kelvinToCelsius } from '../../../helper/convert';
 
 function Hourly({ weatherData }) {
     let hourly = initWeatherData.hourly;
@@ -9,7 +10,8 @@ function Hourly({ weatherData }) {
         hourly = [...weatherData.hourly];
     };
     console.log('HOURLY: ', hourly);
-
+    // const conversionFunc = toCelsius ? kelvinToCelsius : kelvinToFahrenheit;
+    const conversionFunc = kelvinToFahrenheit;
     return (
         <div className='hourly-section'>
             <div className='hourly-title'>
@@ -21,20 +23,20 @@ function Hourly({ weatherData }) {
                         <div key={hour.id} className='hourly-extra-info'>
                             <div className='hourly-extra-info-primary'>
                                 <div className='hourly-weather-icon'><BsFillCloudSnowFill size='2rem'/></div>
-                                <div className='hourly-temp'>5°</div>
-                                <div className='hourly-temp-description'>Cloudy</div>
+                                <div className='hourly-temp'>{Math.round(conversionFunc(hour.temp))}°</div>
+                                <div className='hourly-temp-description'>{hour.description}</div>
                             </div>
                             <div className='hourly-extra-info-secondary'>
                                 <div className='hourly-humidity'>
                                     <WiHumidity />
-                                    <p>64%</p>
+                                    <p>{hour.humidity}%</p>
                                 </div>
                                 <div className='hourly-wind'>
                                     <BsWind/>
-                                    <p>12 km/h</p>
+                                    <p>{(hour.wind * 2.23694).toFixed(2)} mph</p>
                                 </div>
                             </div>
-                            <div className='hourly-time'>9 PM</div>
+                            <div className='hourly-time'>{hour.time}</div>
                         </div>  
                     );
                 })}
